1aeb83f972e2684554eeee58900c47be076f56c8
Ariel <rodriargueta79@gmail.com>
2025-10-29 16:42:40 -0600

    Estructura y conexion
    Creacion de la estructura principal y conexion con la base de datos

---
+++ Proyecto/Sistema.Datos/Conexion.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.SqlClient;
+
+namespace Sistema.Datos
+{
+    public class Conexion
+    {
+        private string DB;
+        private string SV;
+        private static Conexion CON = null;
+        private Conexion()
+        {
+            this.DB = "SistemaEscolar";
+            this.SV = "localhost";
+        }
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Cadena = new SqlConnection();
+            try
+            {
+                Cadena.ConnectionString = $"Server={this.SV};Database={this.DB};Integrated Security=SSPI;";
+            }
+            catch (SqlException ex)
+            {
+                Cadena = null;
+                throw ex;
+                throw new System.Exception(ex.Message);
+            }
+            return Cadena;
+        }
+        public static Conexion ObtenerInstancia()
+        {
+            if (CON == null)
+            {
+                CON = new Conexion();
+            }
+            return CON;
+        }
+    }
+}
